aaa0f9410177652c4654c661ade707983b34cf21
Michal <mkbielawski@gmail.com>
2026-06-05 19:43:41 +0200

    add swagger

--- src/interviewer/adapter/input/api/interviewer.controller.ts
+++ src/interviewer/adapter/input/api/interviewer.controller.ts
@@ -5,7 +5,9 @@ import {
   toResponse,
 } from 'src/interviewer/adapter/input/api/response/interviewer.response';
 import { InterviewerRequest } from 'src/interviewer/adapter/input/api/request/interviewer.request';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('v1')
 @Controller({ path: 'interviewers', version: ['1'] })
 export class InterviewerController {
   constructor(private readonly interviewerService: InterviewerService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import {
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 import configuration from 'src/infrastructure/config/app.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const setupPipes = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+};
+
+const setupVersioning = (app: INestApplication) => {
   app.enableVersioning({ type: VersioningType.URI });
+};
+
+const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Skillfeedback API')
+    .setDescription('Skillfeedback API description')
+    .setVersion('1.0')
+    .addTag('v1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+};
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api', {
+    exclude: ['swagger', 'swagger-json'],
+  });
+
+  setupPipes(app);
+  setupVersioning(app);
+  setupSwagger(app);
+
   await app.listen(configuration().port);
-}
+};
+
 void bootstrap();
